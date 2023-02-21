ed9581c3dd5584da01514dcc4ed86c3208526e79
Ahmad Danun <ahmadanun1@gmail.com>
2023-02-21 21:19:07 +0700

    Delete updir

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -55,7 +55,6 @@ nvim_tree.setup {
         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
         { key = "h", cb = tree_cb "close_node" },
         { key = "v", cb = tree_cb "vsplit" },
-        { key = "u", action = "dir_up" },
         { key = "cd", action = "cd" },
       },
     },
